748eb202b4cb987d1f5dfb00995606dfa08d8da0
Charles <charlesiddle99@gmail.com>
2020-07-22 15:19:53 +0300

    added vcs

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const category = [
+  { key: 'one bedroom', text: 'one bedroom', value: 'one bedroom' },
+  { key: 'two bedroom', text: 'two bedroom', value: 'two bedroom' },
+  { key: 'three bedroom', text: 'three bedroom', value: 'three bedroom' },
+  { key: 'Four bedroom', text: 'Four bedroom', value: 'Four bedroom' },
+  { key: 'five bedroom', text: 'five bedroom', value: 'five bedroom' },
+  { key: 'Bedsitter', text: 'Bedsitter', value: 'Bedsitter' }
+];
---
+++ client-app/src/features/home/HomePage.tsx
@@ -0,0 +1,48 @@
+import React, { useContext, Fragment } from 'react';
+import { Container, Segment, Header, Button, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { RootStoreContext } from '../../app/stores/rootStore';
+import LoginForm from '../user/LoginForm';
+import RegisterForm from '../user/RegisterForm';
+
+const HomePage = () => {
+  const token = window.localStorage.getItem('jwt');
+  const rootStore = useContext(RootStoreContext);
+  const { user, isLoggedIn } = rootStore.userStore;
+  const {openModal} = rootStore.modalStore;
+  return (
+    <Segment inverted textAlign='center' vertical className='masthead'>
+      <Container text>
+        <Header as='h1' inverted>
+          <Image
+            size='massive'
+            src='/assets/logo.svg'
+            alt='logo'
+            style={{ marginBottom: 12 }}
+          />
+          SokoMarket
+        </Header>
+        {isLoggedIn && user && token ? (
+          <Fragment>
+            <Header as='h2' inverted content={`Welcome back ${user.displayName}`} />
+            <Button as={Link} to='/activities' size='huge' inverted>
+              Explore marketplace!
+            </Button>
+          </Fragment>
+        ) : (
+          <Fragment>
+          <Header as='h2' inverted content={`Welcome to SokoMarket`} />
+          <Button onClick={() => openModal(<LoginForm />)} size='huge' inverted>
+            Login
+          </Button>
+          <Button onClick={() => openModal(<RegisterForm />)} size='huge' inverted>
+            Register
+          </Button>
+        </Fragment>
+        )}
+      </Container>
+    </Segment>
+  );
+};
+
+export default HomePage;
